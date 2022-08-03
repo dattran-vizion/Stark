1b7dcaae3a7418a7dca65082a0ebb668b09c02d8
Xingtitan <dat.tran@vizion.space>
2022-08-03 07:09:36 +0700

    Rebuild

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const hamburgerIcon = document.querySelector(".menu-btn");
+const offcanvas = document.querySelector(".offcanvas");
+// const mobileNav = document.querySelector(".mobile-nav");
+const body = document.getElementsByTagName("body")[0];
 
 hamburgerIcon.addEventListener("click", isToggledMenu);
 
 function isToggledMenu() {
   hamburgerIcon.classList.toggle("isToggled");
+  offcanvas.classList.toggle("isToggled");
+  // mobileNav.classList.toggle("isToggled");
+  body.classList.toggle("isToggled");
 }
